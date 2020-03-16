7a6738f2ffafeca9c66d899bde27a556f7bc39bd
Emanuele Panizon <emanuele.panizon@uni-konstanz.de>
2020-03-16 10:52:14 +0100

    Added environment for ROD manipulation and its fortran module

---
+++ evolve_fortran_rod.f95
@@ -0,0 +1,277 @@
+subroutine evolve_MD_rod(X,Y,Theta, Xrod, Yrod, act, Rm, Rr, dt, &
+                     nsteps, tor, vel, N, Nrod, new_XYT, new_XY_rod)
+! ===========================================
+! gets observables and rewards from positions
+! ===========================================
+    implicit none
+    real,    intent(in) :: X(N), Y(N), Theta(N)
+    real,    intent(in) :: Xrod(Nrod), Yrod(Nrod)
+    integer, intent(in) :: act(N)
+    integer, intent(in) :: N, Nrod, nsteps
+    real,    intent(in) :: Rm, Rr, tor, vel, dt
+    ! =======================================
+    real , intent(out) :: new_XYT(N,3), new_XY_rod(Nrod,2)
+    ! =======================================
+    real :: velX(N), velY(N), velR(N)
+    real :: velXrod, velYrod, torquerod, rodXcm, rodYcm, rodtheta
+    integer :: i, j, it
+    real :: dx, dy, r2, drodx, drody
+    ! =======================================
+    ! force parameters
+    real :: eps = 1., ss = 4.8, ss2, ss6, ss12, ff
+    ! =======================================
+    ! rod parameters
+    real :: massRod = 1, Irod = 0.d0
+    ! =======================================
+    ss2  = ss*ss
+    ss6  = ss2*ss2*ss2
+    ss12 = ss6*ss6
+    ! =======================================   
+
+    new_XYT(:,1) = X
+    new_XYT(:,2) = Y
+    new_XYT(:,3) = Theta
+    
+    new_XY_rod(:,1) = Xrod
+    new_XY_rod(:,2) = Yrod
+
+    rodXcm = SUM(new_XY_rod(:,1))/Nrod
+    rodYcm = SUM(new_XY_rod(:,2))/Nrod
+    rodtheta = atan2(new_XY_rod(Nrod,2)-new_XY_rod(1,2), new_XY_rod(Nrod,1)-new_XY_rod(1,1))
+
+    do i = 1, Nrod
+        Irod = Irod + ((i-(Nrod+1)/2.0)*5.0*massRod)**2
+    enddo
+
+    do it = 1, nsteps
+
+        velX = 0.d0
+        velY = 0.d0
+        velR = 0.d0
+
+        velXrod = 0.d0
+        velYrod = 0.d0
+        torquerod = 0.d0
+
+
+    ! =============================
+    ! thermal motion + activity
+    ! =============================
+
+        do i = 1, N
+            velX(i) = gran()*Rm
+            velY(i) = gran()*Rm
+            velR(i) = gran()*Rr
+
+            if (act(i)>0) then
+                velX(i) = velX(i) + cos(new_XYT(i,3))*vel
+                velY(i) = velY(i) + sin(new_XYT(i,3))*vel
+                if (act(i)>1) then
+                    velR(i) = velR(i) - 2*tor*(act(i)-2.5)
+                endif
+            endif
+         enddo
+
+    ! =============================
+    ! repulsion
+    ! =============================
+
+      do i = 1, N-1
+          do j = i+1, N
+              dx = new_XYT(j,1) - new_XYT(i,1)
+              dy = new_XYT(j,2) - new_XYT(i,2)
+              r2 = dx*dx + dy*dy
+              if (r2 < ss2) then
+                  ff = ss12/(r2**6) - ss6/(r2**3)
+                  ff = 12.*eps*ff/r2
+                  ! ==== DEBUG ====
+                  if (ff > 1) then
+                      ff = 1
+                      print*, 'too much'
+                  endif
+                  ! ===============
+                  velX(i) = velX(i) - ff*dx
+                  velY(i) = velY(i) - ff*dy
+                  velX(j) = velX(j) + ff*dx
+                  velY(j) = velY(j) + ff*dy                  
+              endif 
+          enddo
+      enddo
+
+    ! =============================
+    ! repulsion with rod
+    ! =============================
+
+     do j = 1, Nrod
+         drodx = new_XY_rod(j,1) - rodXcm
+         drody = new_XY_rod(j,2) - rodYcm
+
+         do i = 1, N
+              dx = new_XY_rod(j,1) - new_XYT(i,1)
+              dy = new_XY_rod(j,2) - new_XYT(i,2)
+              r2 = dx*dx + dy*dy
+              if (r2 < ss2) then
+                  ff = ss12/(r2**6) - ss6/(r2**3)
+                  ff = 12.*eps*ff/r2
+                  ! ==== DEBUG ====
+                  if (ff > 1) then
+                      ff = 1
+                      print*, 'too much'
+                  endif
+                  ! ===============
+                  velX(i) = velX(i) - ff*dx
+                  velY(i) = velY(i) - ff*dy
+
+                  velXrod = velXrod + ff*dx
+                  velYrod = velYrod + ff*dy                  
+                  torquerod = torquerod + ff * (drodx*dy-drody*dx)
+
+              endif 
+          enddo
+      enddo
+    
+    ! =============================
+    ! move
+    ! =============================
+
+      do i = 1, N
+          new_XYT(i,1) = new_XYT(i,1) + velX(i)*dt
+          new_XYT(i,2) = new_XYT(i,2) + velY(i)*dt
+          new_XYT(i,3) = new_XYT(i,3) + velR(i)*dt
+      enddo
+
+    ! =============================
+    ! move rod degrees of freedom
+    ! =============================
+
+      rodXcm = rodXcm + dt*velXrod/Nrod/massRod
+      rodYcm = rodYcm + dt*velXrod/Nrod/massRod
+      rodtheta = rodtheta + dt*torquerod/Nrod/Nrod/5 ! FAKE Inertia
+
+    ! =============================
+    ! transform rod 
+    ! =============================
+
+      do i = 1, Nrod
+          new_XY_rod(i,1) = (i-(Nrod+1)/2.0)*cos(rodtheta)*5.0 + rodXcm
+          new_XY_rod(i,2) = (i-(Nrod+1)/2.0)*sin(rodtheta)*5.0 + rodYcm
+      enddo 
+
+    enddo
+
+    return
+
+contains 
+
+    real FUNCTION gran()
+    !     polar form of the Box-Muller transformation
+    !     http://www.taygeta.com/random/gaussian.html
+      implicit none
+      !  real :: rand ! using old generator
+      real :: rnum(2)
+      real :: x1, x2, w, y1
+      do
+    !     x1 = 2.0 * rand() - 1.0 ! using old generator
+    !     x2 = 2.0 * rand() - 1.0 ! using old generator
+        call random_number(rnum)
+        x1 = 2.0 * rnum(1) - 1.0
+        x2 = 2.0 * rnum(2) - 1.0
+        w = x1 * x1 + x2 * x2
+        if( w < 1.0 ) goto 10
+      end do
+      10 continue
+      w = sqrt( (-2.0 * log( w ) ) / w )
+      y1 = x1 * w
+      !  y2 = x2 * w ! there is here a second independent random number for free
+      gran=y1
+      return
+    end function gran
+
+end subroutine
+
+
+subroutine get_o_r_rod(X, Y, Theta, Xrod, Yrod, oldXrod, oldYrod, N, Nrod, Obs, Rew)
+! ===========================================
+! gets observables and rewards from positions
+! ===========================================
+    implicit none
+    real, intent(in)    :: X(N), Y(N), Theta(N)
+    real, intent(in)    :: Xrod(Nrod), Yrod(Nrod)
+    real, intent(in)    :: oldXrod(Nrod), oldYrod(Nrod)
+    integer, intent(in) :: N, Nrod
+    real, intent(out)   :: Obs(N,10), Rew(N)
+    integer :: i, j, n_cone
+    real :: dx, dy, r, dtheta, val, th, cmRod(2), oldcmRod(2), thetaRod, dRod
+    real :: a, b, ss=4.8
+    real, parameter :: PI = 3.14159265358979323846264
+
+    Obs = 0
+    Rew = 0
+  
+    
+    cmRod(1) = SUM(Xrod)/Nrod
+    cmRod(2) = SUM(Yrod)/Nrod
+
+    oldcmRod(1) = SUM(oldXrod)/Nrod
+    oldcmRod(2) = SUM(oldYrod)/Nrod
+
+    dRod = sqrt((oldcmRod(2)-cmRod(2))**2 + (oldcmRod(1)-cmRod(1))**2 )
+
+    thetaRod = atan2(cmRod(2) - oldcmRod(2), cmRod(1) - oldcmRod(1))
+
+
+    ! =============================
+    ! seeing other particles
+    ! =============================
+
+    do i = 1, N-1
+        do j = i+1, N
+            dx = X(j)-X(i)
+            dy = Y(j)-Y(i)
+            r = sqrt(dx*dx + dy*dy)
+            dtheta = atan2(dy,dx)
+            ! i to j
+            th = (dtheta - Theta(i))/2./PI
+            th = th - floor(th + 0.5)
+            n_cone = floor((th + 0.5)*5) + 1
+            val = (6.8/r)**2
+            if ((n_cone < 6) .and. (n_cone>0)) then
+                Obs(i,n_cone) = Obs(i,n_cone)+val
+            endif
+            ! j to i
+            th = (dtheta + PI - Theta(j))/2./PI
+            th = th - floor(th + 0.5)
+            n_cone = floor((th + 0.5)*5) + 1
+            if ((n_cone < 6) .and. (n_cone>0)) then
+                Obs(j,n_cone) = Obs(j,n_cone)+val
+            endif
+        enddo
+    enddo
+
+    ! seeing the rod particles + rewards
+    do i = 1, N
+
+        a = Theta(i)
+        b = thetaRod
+
+        do j = 1, Nrod
+            dx = Xrod(j)-X(i)
+            dy = Yrod(j)-Y(i)
+            r = sqrt(dx*dx + dy*dy)
+            dtheta = atan2(dy,dx)
+            ! particle sees rod 
+            th = (dtheta - Theta(i))/2./PI
+            th = th - floor(th + 0.5)
+            n_cone = floor((th + 0.5)*5) + 1
+            val =  (6.8/r)**2
+            if ((n_cone < 6) .and. (n_cone>0)) then
+                Obs(i,n_cone+5) = Obs(i,n_cone+5)+val
+                Rew(i) = Rew(i) + dRod * cos(a-b) /( ((r-ss*0.8)/ss) )
+           endif
+
+        enddo
+
+
+    enddo
+    return
+end subroutine
---
+++ md_env_fortran_rod.py
@@ -0,0 +1,120 @@
+# ---------------------------------------
+import numpy as np
+import math
+import sys
+from scipy.spatial.distance import cdist
+import time
+import evolve_fortran_rod as evolve
+# ---------------------------------------
+
+# ---------------------------------------
+# EQUIVALENT OF GYM FOR MD ENVIRONMENT
+# ---------------------------------------
+
+#===============================================================================
+# class MDEnv(gym.Env):
+# 
+#     def __init__(self):
+# 
+#   def step(self, action):
+#     ...
+#   def reset(self):
+#     ...
+#   def render(self, mode='human'):
+#     ...
+#   def close(self):
+#     ...
+#===============================================================================
+
+class MD_ROD():
+    def __init__(self, index=0, N=10, Nrod=3, size=10, steps=20, vel=0.5, dt=0.2, torque=25.0):
+
+        self.dt = dt
+        self.vel_prey = vel
+        self.torque_prey = 1.0 / 350.0 * torque # this is Dr * Gamma / kT = 1/350 * 10kT / kT (which is Torque)
+        
+        self.N = N
+        self.Nrod = Nrod
+        self.size = size
+        self.n_MD_steps =steps
+        self.rewards = np.zeros(N)
+
+        self.Dt = 0.014
+        self.Dr = 1.0 / 350.0
+        self.Rm = math.sqrt(2*self.Dt/self.dt)
+        self.Rr = math.sqrt(2*self.Dr/self.dt)
+
+        self.filexyz='traj'+str(index)+'.xyz'
+        self.particles, self.rod = self.reinitialize_random_for_MD(index)
+        self.old_rod = self.rod
+# --------------------------
+# INITIALIZE RANDOMLY X,Y IN A BOX [-10:10,-10:10] AND THETA [-pi, pi] 
+    def reinitialize_random_for_MD(self, index):
+        sN = np.int(np.sqrt(self.N))+1
+        particles = np.random.rand(self.N, 3)*[0.0,0.0,2*np.pi]
+        pos = np.array([[i,j,0] for i in np.arange(-sN//2-1,sN//2+1) for j in np.arange(-sN//2-1,sN//2+1)])
+        for i in range(sN):
+            for j in range(sN):
+                if (i*sN+j+1) < self.N :
+                  oo = np.random.randint(pos.shape[0])
+                  particles[i*sN+j+1,:] += pos[oo]*5.0
+                  pos = np.delete(pos, oo, axis=0)
+        rod = np.array([[(sN+1.0)*5.0, (i-(self.Nrod-1)/2.0)*5] for i in np.arange(self.Nrod)])
+        open(self.filexyz, "w") 
+        return particles, rod
+
+  # PRINT TRAJECTORY
+    def print_xyz(self):
+        p = self.particles
+        rod = self.rod
+        xyz_file = open(self.filexyz, "a") 
+        xyz_file.write('\n\n')
+        for i in range(self.N):
+            xyz_file.write('0 {} {} 0.0 {} {} {}\n'.format( p[i,0], p[i,1], np.cos(p[i,2]), np.sin(p[i,2]), self.rewards[i] ) )
+        for i in range(self.Nrod):
+            xyz_file.write('1 {} {} 0.0 0.0 0.0 0.0\n'.format(rod[i,0], rod[i,1]) )
+
+  # CALLS THE FORTRAN SUBROUTINE FOR OBS AND REWARDS IN PRESENCE OF A ROD
+    def get_o_r_rod_fortran(self):
+        t0 = time.time()    
+        p = self.particles 
+        r = self.rod
+        olr = self.old_rod
+        obs, rewards = evolve.get_o_r_rod(p[:,0],p[:,1],p[:,2], r[:,0], r[:,1], olr[:,0],olr[:,1], self.N, self.Nrod)
+        # DEGUB
+        self.rewards = rewards
+        return obs, rewards
+
+    def get_obs_rewards(self):
+        return self.get_o_r_rod_fortran()
+
+    def evolve_MD(self, action):
+        t0 = time.time()
+        done = False
+        X = self.particles[:,0]        
+        Y = self.particles[:,1]
+        T = self.particles[:,2]
+        Xrod = self.rod[:,0]
+        Yrod = self.rod[:,1]
+        self.particles, self.rod = evolve.evolve_md_rod(X, Y, T, Xrod, Yrod, action, self.Rm, self.Rr, self.dt, self.n_MD_steps, self.torque_prey, self.vel_prey, self.N, self.Nrod)
+        obs, rewards = self.get_obs_rewards()
+        self.old_rod = self.rod
+        return obs, rewards, done, {}
+#
+# ------------------------------------
+# End of class MD
+# ------------------------------------
+
+# CHECK FOR CONE OF SIGHT
+def minus_0p5pi_to_1p5pi_mod(theta1):
+    return (theta1 + math.pi)%(2*math.pi)-math.pi/2.0
+
+def get_cone_sight(A, B):
+    dx = B[0] - A[0]
+    dy = B[1] - A[1]
+    dist = math.sqrt(dx*dx + dy*dy)
+    dist_theta = math.atan2(dy, dx)
+    rel_theta = ((dist_theta - A[2])/math.pi + 1.0)%(2.0)-0.5
+    n_cone = math.floor(rel_theta*5) 
+    return int(n_cone), dist
+	
