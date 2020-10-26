d5bde361b4ee1f274aea20a38dc59ad465b9b871
Robert Löffler <robert.loeffler@uni-konstanz.de>
2020-10-26 17:40:21 +0100

    jobscript for SCC-KN (hardly tested)

---
+++ jobscript-konstanz.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+## Name (for logging and display)
+#$ -N rl-food-ratio-test
+
+## Maximum Memory
+# #$ -l h_vmem=no idea ...
+
+## Logfile configuration (folder must exist!)
+#$ -j y
+#$ -o /data/scc/robert.loeffler/Logs/
+
+## Number of tasks
+#$ -t 0-2
+
+## Send email
+## few large jobs, so success emails are also welcome
+#$ -m eas
+#$ -M robert.loeffler@uni-konstanz.de,epanizon@gmail.com
+
+#-------------------
+
+# The ratios to simulate in the different tasks
+ratios=(0.0 0.95 1.0)
+
+code=`pwd`
+data=/data/scc/robert.loeffler/2020-10-26-rl-food-ratio-test/ratio-${ratios[$SGE_TASK_ID]}
+
+mkdir -p $data
+cd $data
+
+python3 $code/learning_GROUP_FOOD_dOBS.py ${ratios[$SGE_TASK_ID]} > results.dat
+
+cd $code
+
