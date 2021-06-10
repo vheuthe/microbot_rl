8229ea1e74f7f017c96324b61474a7c221b31dcf
= <=>
2021-06-10 15:59:58 +0200

    Quick and dirty copy of learning_rod.py to test passive instead of non-existent particles in diffRew while not interferring with running simulation

--- jobscript.sh
+++ jobscript.sh
@@ -33,12 +33,12 @@ tensorflow.config.threading.set_intra_op_parallelism_threads(2)
 # --- Parse parameters and start simulation ---
 
 import os
-import learning_rod
+import learning_rod_temp
 
 task_id = int($SGE_TASK_ID)
 
 job_dir = os.path.abspath('$JOB_DIR')
 
-learning_rod.do_array_task(task_id, job_dir)
+learning_rod_temp.do_array_task(task_id, job_dir)
 
 ENDOFPYTHON
