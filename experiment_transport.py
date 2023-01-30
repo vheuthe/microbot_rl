0791a05861fc3e4028565f1e2cc214efb0fbe64b
Veit-Lorenz Heuthe <veit-lorenz.heuthe@uni-konstanz.de>
2023-01-30 09:51:05 +0000

    Revert "Debugged"
    This reverts commit 15fa34d2f232ebc0ff5c7fbc687bf82c7b98c000

--- experiment_transport.py
+++ experiment_transport.py
@@ -31,9 +31,7 @@ def serve_experiment():
             parameters.update(json.load(paramfile))
     else:
         parameters = learning_rod.default_parameters
-
-    # Mark that this is experiment
-    parameters['exp_or_sim'] = 'exp'
+        parameters['exp_or_sim'] = 'exp'
 
     # specific experimental parameters have highest priority
     parameters.update(exp_parameters)
